0b0f2a2a28d6f1556bb719dd91b57d089683c07a
Marcos Oliveira <mspol23@gmail.com>
2024-07-21 19:32:52 -0300

    melhor solução (com interface) no HEAD do repositório

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,13 +1,8 @@
 package model.services;
 
-<<<<<<< HEAD
-public class BrazilTaxService extends TaxService {
-=======
 public class BrazilTaxService implements TaxService {
->>>>>>> 92baee0 (Solução com interface. Indicada.)
 	
-	@Override
-	public Double tax (double basicPayment) {
+	public double tax (double basicPayment) {
 		if(basicPayment <= 100.0) {
 			return basicPayment * 0.2;
 		}
--- src/model/services/TaxService.java
+++ src/model/services/TaxService.java
@@ -1,16 +1,6 @@
 package model.services;
 
-<<<<<<< HEAD
-public class TaxService {
-
-	public Double tax(double amount) {
-		return null;
-	}
-
-=======
 public interface TaxService {
 
 	double tax(double amount);
-	
->>>>>>> 92baee0 (Solução com interface. Indicada.)
 }
